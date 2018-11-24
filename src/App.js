bbf853872ce383715197e8771f2ebccc38fb6e86
Lina <40521260+Lina-ElBadrawy@users.noreply.github.com>
2018-11-24 12:09:31 +0200

    Adding Api + List binding started

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+  
+  constructor() {
+    super();
+    this.state = {
+      booksList:[]
+      /**
+       * TODO: Instead of using this state variable to keep track of which page
+       * we're on, use the URL in the browser's address bar. This will ensure that
+       * users can use the browser's back and forward buttons to navigate between
+       * pages, as well as provide a good URL they can bookmark and share.
+       */
+     // showSearchPage: false
+    }
+  
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      console.log(books)
+      this.setState({ booksList: books })
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-           <ListBooks />
+           <ListBooks books={this.state.booksList} />
           
         )} />
         <Route path='/search' render={() => (
